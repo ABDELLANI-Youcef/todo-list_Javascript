2475b13fc3f932b846f5290531cfdd68dbd9682a
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-01-19 22:37:59 +0100

    tests project setters

--- src/tests/project.test.js
+++ src/tests/project.test.js
@@ -23,3 +23,20 @@ test('Tasks getter returns project tasks',()=>{
   expect(project.tasks instanceof Array).toBeTruthy();
   expect(project.tasks.length).toBe(0);
 });
+
+test('Title setters modifies project title',()=>{
+  project.title = 'modified title';
+  expect(project.title).toBe('modified title');
+});
+
+test('addTask method adds task to the project',()=>{
+  project.addTask(task);
+  expect(project.tasks.length).toBe(1);
+  expect(project.taskCounter).toBe(1);
+});
+
+test('removeTask method removes task from the project',()=>{
+  project.removeTask(task);
+  expect(project.tasks.length).toBe(0);
+});
+

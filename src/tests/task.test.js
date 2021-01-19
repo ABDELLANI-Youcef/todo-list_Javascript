f9a8c57b3fa1dc22424d8e2da17a10fe0c7c2a47
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-01-19 15:07:08 -0600

    Testing Task setters methods

--- src/tests/task.test.js
+++ src/tests/task.test.js
@@ -26,10 +26,39 @@ test('dueDate getter returns task dueDate', () => {
   expect(task.dueDate).toBe('2021-1-19');
 });
 
+test('priority getter returns task priority', () => {
+  expect(task.priority).toBe('low');
+});
+
 test('status getter returns task status', () => {
   expect(task.status).toBe('To-do');
 });
 
 test('formatedDueDate returns a formated task dueDate', () => {
   expect(task.formatedDueDate() instanceof Date).toBeTruthy();
+});
+
+test('Title setter modifies task title', () => {
+  task.title = 'modified title';
+  expect(task.title).toBe('modified title');
+});
+
+test('description setter modifies task description', () => {
+  task.description = 'modified description';
+  expect(task.description).toBe('modified description');
+});
+
+test('dueDate setter modifies task dueDate', () => {
+  task.dueDate = '2022-2-20';
+  expect(task.dueDate).toBe('2022-2-20');
+});
+
+test('priority setter modifies task priority', () => {
+  task.priority = 'hight';
+  expect(task.priority).toBe('hight');
+});
+
+test('status setter modifies task status', () => {
+  task.status = 'in-progress';
+  expect(task.status).toBe('in-progress');
 });
